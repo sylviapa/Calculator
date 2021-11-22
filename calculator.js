d150f37120a325f8e014fc45117b8144211a561a
Sylvia Pan <panxiaoyuns@gmail.com>
2021-11-22 12:32:41 -0800

    Final change

--- calculator.js
+++ calculator.js
@@ -1,50 +1,41 @@
-
-
 let username;
 let num1;
 let num2;
 let result;
 let user_continue;
 
-
-
 function calculate(num1, num2) {
     result = num1 + num2;
     alert('The sum of your two numbers is: '+result);
 }
 
-document.getElementById("demo1").addEventListener("click", welFunction);
+// start the function when user click the button
+document.getElementById("demo").addEventListener("click", welFunction);
 
 function welFunction() {
-   alert('Welcome to my site!');
-   username = prompt("What is your name?");
-   alert('Welcome '+ username);
+    alert('Welcome to my site!');
+    username = prompt("What is your name?");
+    alert('Welcome '+username+'!');
+
+// repeat the calculator until user input N
+    while (user_continue!='N'){
+        num1 = Number(prompt("Please enter the first number: "));
+        
+        //check if input is valid number
+        while(isNaN(num1)==true){num1=Number(prompt('Please enter a valid number:'))};
+
+        num2 = Number(prompt("Please enter the second number: "));
+
+        while(isNaN(num2)==true){num2=Number(prompt('Please enter a valid number:'))};
+    
+        calculate(num1,num2);
+
+        if (result>10) {alert('That is a big number.')} else {alert('That is a small number.')};
+
+        user_continue = prompt("Do you want to add two numbers again? Enter anything to continue and N to quit:");
+        
+        if(user_continue=='N'){
+            alert("Thank the visitor for using the program!")
+        }
+    }
 }
-
-document.getElementById("demo2").addEventListener("click", calFunction);
-
-function calFunction() {
-    alert('a')
-}
-/*
-alert('Welcome to my site!');
-
-username = prompt("What is your name?");
-
-alert('Welcome '+ username);
-
-while (user_continue!='N'){
-
-num1 = Number(prompt("Please enter the first number: "));
-num2 = Number(prompt("Please enter the second number: "));
-
-calculate(num1, num2);
-
-if (result>10) {alert('That is a big number')} else {alert('That is a small number')};
-
-user_continue = prompt("Please enter Y if you want to continue or N to quit: ");
-}
-
-*/
-
-
